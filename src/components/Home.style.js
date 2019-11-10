3166184e3a257d5c8e27018b69efad99923af8b7
Alexander Tickle <alexander_tickle@intuit.com>
2019-11-10 08:13:37 -0800

    fix header

--- src/components/Home.style.js
+++ src/components/Home.style.js
@@ -2,7 +2,7 @@ export const HEADER = {
     padding: 10,
     border: '1px solid black',
     backgroundColor: '#3a4ba7',
-    height: 55,
+    height: 65,
     color: 'white',
     fontSize: 16
 };
